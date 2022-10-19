ec8522f293ddec1f7e7f1471ed6a9d54ae6e658f
KingZ12ultimate <84376912+KingZ12ultimate@users.noreply.github.com>
2022-10-19 21:59:16 +0300

    Instancing

--- Assets/Scripts/Instancer.cs
+++ Assets/Scripts/Instancer.cs
@@ -2,60 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Mesh)), RequireComponent(typeof(Material))]
 public class Instancer : MonoBehaviour
 {
-    [SerializeField]
-    Mesh mesh;
-    [SerializeField]
-    Material material;
-    List<List<Matrix4x4>> matrices = new List<List<Matrix4x4>>();
-    ComputeBuffer argsBuffer;
+    public Mesh mesh;
+    public Material material;
     ComputeBuffer positionsBuffer;
-    ComputeShader positionsShader;
-    
+    GraphicsBuffer commandBuffer;
+    GraphicsBuffer.IndirectDrawIndexedArgs[] commandData;
+
     [Range(0, 10000)]
-    public int numInstances;
-    private uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+    public int numInstances = 10;
+    [Range(0, 100)]
+    public float radius = 50;
+
+    private void Awake()
+    {
+        commandData = new GraphicsBuffer.IndirectDrawIndexedArgs[1];
+    }
 
     private void OnEnable()
     {
-        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
-        positionsBuffer = new ComputeBuffer(numInstances, 4 * sizeof(int));
-        Vector4[] positions = new Vector4[numInstances];
-        for (int i = 0; i < numInstances; i++)
-        {
-            float angle = Random.Range(0.0f, Mathf.PI * 2.0f);
-            float distance = Random.Range(20.0f, 100.0f);
-            float height = Random.Range(-2.0f, 2.0f);
-            float size = Random.Range(0.05f, 0.25f);
-            positions[i] = new Vector4(Mathf.Sin(angle) * distance, height, Mathf.Cos(angle) * distance, size);
-        }
-        positionsBuffer.SetData(positions);
-        material.SetBuffer("PositionsBuffer", positionsBuffer);
-
-        args[0] = (uint)mesh.GetIndexCount(0);
-        args[1] = (uint)numInstances;
-        args[2] = (uint)mesh.GetIndexStart(0);
-        args[3] = (uint)mesh.GetBaseVertex(0);
-
-        argsBuffer.SetData(args);
+        positionsBuffer = new ComputeBuffer(numInstances, 4 * sizeof(float));
+        commandBuffer = new GraphicsBuffer(GraphicsBuffer.Target.IndirectArguments, 1, GraphicsBuffer.IndirectDrawIndexedArgs.size);
     }
 
     private void OnDisable()
     {
-        if (positionsBuffer != null)
-            positionsBuffer.Release();
+        positionsBuffer?.Release();
         positionsBuffer = null;
 
-        if (argsBuffer != null)
-            argsBuffer.Release();
-        argsBuffer = null;
+        commandBuffer?.Release();
+        commandBuffer = null;
+    }
+
+    private void UpdatePositions()
+    {
+        Vector4[] positions = new Vector4[numInstances];
+        for (int i = 0; i < numInstances; i++)
+        {
+            Vector3 pos = Random.insideUnitSphere * radius;
+            positions[i] = new Vector4(pos.x, pos.y, pos.z, 1f);
+        }
+        positionsBuffer.Release();
+        positionsBuffer = new ComputeBuffer(numInstances, 4 * sizeof(float));
+        positionsBuffer.SetData(positions);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Graphics.DrawMeshInstancedIndirect(mesh, 0, material, )
+        UpdatePositions();
+        RenderParams renderParams = new RenderParams(material);
+        renderParams.worldBounds = new Bounds(Vector3.zero, 1000 * Vector3.one);
+        renderParams.matProps = new MaterialPropertyBlock();
+        renderParams.matProps.SetBuffer("PositionsBuffer", positionsBuffer);
+        renderParams.matProps.SetMatrix("_ObjectToWorld", Matrix4x4.Translate(new Vector3(-4.5f, 0, 0)));
+        commandData[0].instanceCount = (uint)numInstances;
+        commandData[0].indexCountPerInstance = mesh.GetIndexCount(0);
+        commandBuffer.SetData(commandData);
+        Graphics.RenderMeshIndirect(renderParams, mesh, commandBuffer);
     }
 }
---
+++ Assets/Shaders/InstancingShader.shader
@@ -0,0 +1,51 @@
+Shader "Custom/Instancing Shader"
+{
+    Properties
+    {
+        _MainColor ("Color", Color) = (1, 0, 0, 1)
+    }
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            #define UNITY_INDIRECT_DRAW_ARGS IndirectDrawIndexedArgs
+            #include "UnityIndirect.cginc"
+
+            struct VertexData
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float4 color : COLOR0;
+            };
+
+            float4 _MainColor;
+            uniform StructuredBuffer<float4> PositionsBuffer;
+
+            v2f vert (VertexData v, uint svInstanceID : SV_InstanceID)
+            {
+                InitIndirectDrawArgs(0);
+                v2f o;
+                uint cmdID = GetCommandID(0);
+                uint instanceID = GetIndirectInstanceID(svInstanceID);
+                o.vertex = UnityObjectToClipPos(v.vertex + PositionsBuffer[instanceID]);
+                o.color = _MainColor;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return i.color;
+            }
+            ENDCG
+        }
+    }
+}
