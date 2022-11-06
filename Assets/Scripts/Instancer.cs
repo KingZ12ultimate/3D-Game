30ab05564ae4e31ef6b24df1e1bd6356ae14a34c
KingZ12ultimate <84376912+KingZ12ultimate@users.noreply.github.com>
2022-11-06 10:28:17 +0200

    YAY YAY

--- Assets/Scripts/Instancer.cs
+++ Assets/Scripts/Instancer.cs
@@ -4,47 +4,37 @@ using UnityEngine;
 
 public class Instancer : MonoBehaviour
 {
-    ComputeBuffer intBuffer, voteBuffer, scanBuffer, culledIntBuffer,
-        groupSumsBuffer, scannedGroupSumsBuffer;
-    public ComputeShader cullGrassShader;
+    ComputeBuffer inputBuffer, voteBuffer, scanBuffer, groupSumsBuffer, scannedGroupSumsBuffer, outputBuffer;
+    public ComputeShader cullShader;
 
     [SerializeField, Range(1, 1000)]
     private int numInstances = 8;
 
-    private int numThreadGroups;
-    private int numGroupScanThreadGroups;
-
     private int[] values;
 
+    private int numThreadGroups, numVoteThreadGroups, numGroupScanThreadGroups;
+
     #region Cull Grass Shader IDs
-    static readonly int intBufferID = Shader.PropertyToID("IntBuffer");
+    static readonly int inputBufferID = Shader.PropertyToID("Input");
     static readonly int voteBufferID = Shader.PropertyToID("VoteBuffer");
     static readonly int scanBufferID = Shader.PropertyToID("ScanBuffer");
     static readonly int groupSumsBufferID = Shader.PropertyToID("GroupSumsBuffer");
-    static readonly int groupSumsInBufferID = Shader.PropertyToID("GroupSumsInBuffer");
-    static readonly int groupSumsOutBufferID = Shader.PropertyToID("GroupSumsOutBuffer");
-    static readonly int culledIntBufferID = Shader.PropertyToID("CulledIntBuffer");
-    static readonly int matrixVPID = Shader.PropertyToID("MATRIX_VP");
-    static readonly int cameraPositionID = Shader.PropertyToID("CameraPosition");
-    static readonly int distanceID = Shader.PropertyToID("Distacne");
+    static readonly int groupSumsBufferInID = Shader.PropertyToID("GroupSumsBufferIn");
+    static readonly int groupSumsBufferOutID = Shader.PropertyToID("GroupSumsBufferOut");
+    static readonly int outputBufferID = Shader.PropertyToID("Output");
     static readonly int numGroupsID = Shader.PropertyToID("NumGroups");
     #endregion
 
     private void Awake()
     {
         values = new int[numInstances];
-        for (int i = 0; i < numInstances; i++)
-        {
-            values[i] = Random.Range(0, 21);
-        }
     }
 
     private void OnEnable()
     {
-        intBuffer = new ComputeBuffer(numInstances, sizeof(int));
-        voteBuffer = new ComputeBuffer(numInstances, sizeof(int));
-        scanBuffer = new ComputeBuffer(numInstances, sizeof(int));
-        culledIntBuffer = new ComputeBuffer(numInstances, sizeof(int));
+        inputBuffer = new ComputeBuffer(numInstances, sizeof(int));
+        voteBuffer = new ComputeBuffer(numInstances, sizeof(uint));
+        scanBuffer = new ComputeBuffer(numInstances, sizeof(uint));
 
         numThreadGroups = Mathf.CeilToInt(numInstances / 128f);
         if (numThreadGroups > 128)
@@ -60,78 +50,33 @@ public class Instancer : MonoBehaviour
                 numThreadGroups++;
         }
 
-        numGroupScanThreadGroups = Mathf.CeilToInt(numInstances / 1024f);
-        groupSumsBuffer = new ComputeBuffer(numThreadGroups, sizeof(int));
-        scannedGroupSumsBuffer = new ComputeBuffer(numThreadGroups, sizeof(int));
+        groupSumsBuffer = new ComputeBuffer(numGroupScanThreadGroups, sizeof(uint));
+        scannedGroupSumsBuffer = new ComputeBuffer(numInstances, sizeof(uint));
+        voteBuffer = new ComputeBuffer(numInstances, sizeof(uint));
 
-        CullGrass();
-
-        int[] cVals = new int[numInstances];
-        culledIntBuffer.GetData(cVals);
-        for (int i = 0; i < numInstances; i++)
-        {
-            if (cVals[i] >= 10)
-            {
-                Debug.Log("Bitch!");
-            }
-        }
     }
 
     private void OnDisable()
     {
-        intBuffer?.Release();
+        inputBuffer?.Release();
         voteBuffer?.Release();
-        scanBuffer?.Release();
-        culledIntBuffer?.Release();
-        groupSumsBuffer?.Release();
-        scannedGroupSumsBuffer?.Release();
-        intBuffer = null;
+        inputBuffer = null;
         voteBuffer = null;
-        scanBuffer = null;
-        culledIntBuffer = null;
-        groupSumsBuffer = null;
-        scannedGroupSumsBuffer = null;
     }
 
-    private void CullGrass()
+    private void Start()
     {
-        // Set global variables
-        cullGrassShader.SetInt(numGroupsID, numThreadGroups);
-
-        // Vote on positions
-        cullGrassShader.SetBuffer(0, intBufferID, intBuffer);
-        cullGrassShader.SetBuffer(0, voteBufferID, voteBuffer);
-        cullGrassShader.Dispatch(0, numThreadGroups, 1, 1);
-
-        int[] v = new int[voteBuffer.count];
-        voteBuffer.GetData(v);
-
-        // Scan votes
-        cullGrassShader.SetBuffer(1, voteBufferID, voteBuffer);
-        cullGrassShader.SetBuffer(1, scanBufferID, scanBuffer);
-        cullGrassShader.SetBuffer(1, groupSumsBufferID, groupSumsBuffer);
-        cullGrassShader.Dispatch(1, numThreadGroups, 1, 1);
-
-        int[] s = new int[scanBuffer.count];
-        scanBuffer.GetData(s);
-
-        // Scan group sums
-        //cullGrassShader.SetBuffer(2, groupSumsInBufferID, groupSumsBuffer);
-        //cullGrassShader.SetBuffer(2, groupSumsOutBufferID, scannedGroupSumsBuffer);
-        //cullGrassShader.Dispatch(2, numGroupScanThreadGroups, 1, 1);
-
-        // Compact
-        cullGrassShader.SetBuffer(3, intBufferID, intBuffer);
-        cullGrassShader.SetBuffer(3, voteBufferID, voteBuffer);
-        cullGrassShader.SetBuffer(3, scanBufferID, scanBuffer);
-        cullGrassShader.SetBuffer(3, groupSumsBufferID, scannedGroupSumsBuffer);
-        cullGrassShader.SetBuffer(3, culledIntBufferID, culledIntBuffer);
-        cullGrassShader.Dispatch(3, numThreadGroups, 1, 1);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < numInstances; i++)
+            values[i] = Random.Range(-10, 11);
+        inputBuffer.SetData(values);
+        cullShader.SetBuffer(0, inputBufferID, inputBuffer);
+        cullShader.SetBuffer(0, outputBufferID, voteBuffer);
+        cullShader.Dispatch(0, 1, 1, 1);
     }
 }
